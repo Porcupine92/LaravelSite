b3dd2c6c670ef8e7097b9ba01b3fa4587509c3f8
Porcupine92 <szymonjozefowicz1992@gmail.com>
2020-12-08 15:59:30 +0100

    learnig how to writing routes

---
+++ resources/views/route/viewParam.blade.php
@@ -0,0 +1,4 @@
+<ul></ul>
+    <li>Parametr 1: {{ $param1 }}</li>
+    <li>Parametr 2: {{ $name }}</li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,34 @@ Route::get('/hello/{name}', 'HelloController@hello');
 Route::get('/goodbye/{name}', function (string $name) {
     return 'Goodbye: ' . $name;
 });
+
+// Route::get('/example', function () {
+//     return 'jestem GET';
+// });
+$uri = '/example';
+Route::get($uri, fn () => 'jestem arrow GET');
+Route::post($uri, fn () => 'jestem POST');
+Route::put($uri, fn () => 'jestem PUT');
+Route::patch($uri, fn () => 'jestem PATCH');
+Route::delete($uri, fn () => 'jestem DELETE');
+Route::options($uri, fn () => 'jestem OPTIONS');
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'jestem getem i postem';
+});
+
+Route::any('/all', fn () => 'Dopuszczone wszytskie metody');
+
+Route::view('/view/route', 'route.view');
+
+Route::view(
+    'view/route/var1',
+    'route.viewParam',
+    ['param1' => 'var1 - to nasze dane', 'name' => 'Szymon']
+);
+
+Route::view(
+    'view/route/var2',
+    'route.viewParam',
+    ['param1' => 'inna trasa', 'name' => 'Marcela']
+);
