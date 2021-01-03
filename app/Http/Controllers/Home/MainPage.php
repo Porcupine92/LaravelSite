c73ebb5b68adb32140e8b7c5e07dbc3910a0ca5e
Porcupine92 <szymonjozefowicz1992@gmail.com>
2021-01-03 21:10:08 +0100

    Helper - URL Generator

--- app/Http/Controllers/Home/MainPage.php
+++ app/Http/Controllers/Home/MainPage.php
@@ -5,30 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Game\GameController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class MainPage extends Controller
 {
-    public function __invoke()
-    // public function __invoke(Request $request)
+    public function __invoke(Request $request)
     {
-        // if (Auth::check()) {
-        //     // Jeśli użytkownik jest zalogowany zwróci true
-        //     // dd('Jestem zalogowany');
-        // }
-        // Ogólnie sprawdzaniem zajmuje się middleware auth więc nie ma potrzeby tutaj w akcji tego robić
-        // Ewentualnie w jakiś wyjątkowych sytuacjach
-
         $user = Auth::user();
-        // $user = $request->user();
-        // Pobieranie danych o użytkowniku
-
         $id = Auth::id();
-
-        // dd($id);
-
-        // Auth::logout(); // Wylogowuje użytkownika
 
         return view('home.main', [
             'user' => $user
